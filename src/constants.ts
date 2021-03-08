098b488616859bca1a9f8d7bc40d6235176532fc
ggovajy <ggovajy@gmail.com>
2021-03-08 20:36:47 +0900

    ignore file check

--- src/constants.ts
+++
@@ -1,3 +0,0 @@
-// src/constants.ts
-export const MONO_DB_CONNECTION_STRING =
-  process.env.MONO_DB_CONNECTION_STRING || 'mongodb://ggova:rlawjddus@192.168.0.250/road_addr?authSource=admin&authMechanism=SCRAM-SHA-1';
